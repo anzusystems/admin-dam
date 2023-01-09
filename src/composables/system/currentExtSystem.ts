dacef1117e41f43bc807f748b682b61957995394
Stanislav Volar <stanislav.volar@gmail.com>
2023-01-09 14:04:20 +0100

    current ext system and licence update

--- src/composables/system/currentExtSystem.ts
+++ src/composables/system/currentExtSystem.ts
@@ -2,13 +2,17 @@ import { readonly, ref, watch } from 'vue'
 import { damConfig, damConfigInitialized } from '@/services/DamConfigService'
 import type { ExtSystem } from '@/types/dam/ExtSystem'
 import { fetchExtSystem } from '@/services/api/dam/extSystemApi'
+import { useCurrentUser } from '@/composables/system/currentUser'
 
 const currentExtSystemId = ref(0)
 const currentExtSystem = ref<ExtSystem>()
 
 export const initCurrentExtSystem = () => {
-  watch(currentExtSystemId, async () => {
-    if (currentExtSystemId.value) {
+  const { currentUser } = useCurrentUser()
+
+  watch(currentExtSystemId, async (newValue, oldValue) => {
+    if (newValue !== oldValue && newValue > 0) {
+      currentExtSystem.value = undefined
       currentExtSystem.value = await fetchExtSystem(currentExtSystemId.value)
     }
   })
@@ -17,10 +21,29 @@ export const initCurrentExtSystem = () => {
     if (!damConfigInitialized.value) {
       console.error('Config must be loaded first.')
       reject(false)
+      return
+    }
+    if (!currentUser.value) {
+      console.error('Current user must be loaded first.')
+      reject(false)
+      return
     }
     if (damConfig.settings.allowSelectExtSystem) {
-      console.error('Not yet implemented.')
-      reject(false)
+      if (currentUser.value.selectedLicence) {
+        const foundLicence = currentUser.value.assetLicences.find(
+          (item) => item.id === currentUser.value!.selectedLicence
+        )
+        if (foundLicence) {
+          currentExtSystemId.value = foundLicence.extSystem
+          resolve(true)
+          return
+        }
+      }
+      if (currentUser.value.assetLicences[0]) {
+        currentExtSystemId.value = currentUser.value.assetLicences[0].extSystem
+        resolve(true)
+        return
+      }
     }
     currentExtSystemId.value = damConfig.settings.defaultExtSystemId
     resolve(true)
