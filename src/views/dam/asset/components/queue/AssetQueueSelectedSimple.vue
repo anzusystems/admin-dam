2fbe0f5ef8542197350ba6bb9cb452ccffb99fa9
Stanislav Volar <stanislav.volar@gmail.com>
2023-01-10 10:44:26 +0100

    upload fixes

--- src/views/dam/asset/components/queue/AssetQueueSelectedSimple.vue
+++ src/views/dam/asset/components/queue/AssetQueueSelectedSimple.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AssetQueueItemSimple from '@/views/dam/asset/components/queue/AssetQueueItemSimple.vue'
+import AssetQueueSelectedItemSimple from '@/views/dam/asset/components/queue/AssetQueueSelectedItemSimple.vue'
 import { computed } from 'vue'
 import { useUploadQueuesStore } from '@/stores/dam/uploadQueuesStore'
 import { QUEUE_ID_MASS_EDIT } from '@/services/upload/uploadQueueIds'
@@ -13,12 +13,12 @@ const list = computed(() => {
 
 <template>
   <div class="dam-upload-queue dam-upload-queue--simple w-100 d-flex align-center justify-start">
-    <AssetQueueItemSimple
+    <AssetQueueSelectedItemSimple
       v-for="(item, index) in list"
       :key="item.assetId ? item.assetId : 'null'"
       :index="index"
       :item="item"
       :queue-id="QUEUE_ID_MASS_EDIT"
-    ></AssetQueueItemSimple>
+    ></AssetQueueSelectedItemSimple>
   </div>
 </template>
