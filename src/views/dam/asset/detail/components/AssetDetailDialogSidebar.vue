22350531d09fc8a47f6f62d5effe605a091d9669
Stanislav Volar <stanislav.volar@gmail.com>
2023-01-16 20:01:40 +0100

    show distribution tab for all asset types

--- src/views/dam/asset/detail/components/AssetDetailDialogSidebar.vue
+++ src/views/dam/asset/detail/components/AssetDetailDialogSidebar.vue
@@ -50,7 +50,7 @@ const typeHasDistributions = computed(() => {
       <VTabs v-model="activeTab" class="sidebar-info__tabs">
         <VTab :value="AssetDetailTab.Info">{{ t('coreDam.asset.detail.tabs.info') }}</VTab>
         <VTab :value="AssetDetailTab.ROI" v-if="isImage">{{ t('coreDam.asset.detail.tabs.roi') }}</VTab>
-        <VTab :value="AssetDetailTab.Distribution" v-if="typeHasDistributions">
+        <VTab :value="AssetDetailTab.Distribution">
           {{ t('coreDam.asset.detail.tabs.distribution') }}
         </VTab>
         <VTab :value="AssetDetailTab.Podcast" v-if="isAudio">Podcast</VTab>
@@ -65,9 +65,10 @@ const typeHasDistributions = computed(() => {
         <div class="py-2" v-if="isImage && activeTab === AssetDetailTab.ROI">
           <AssetDetailSidebarROI :is-active="activeTab === AssetDetailTab.ROI" />
         </div>
-        <div class="py-2" v-if="typeHasDistributions && activeTab === AssetDetailTab.Distribution">
+        <div class="py-2" v-if="activeTab === AssetDetailTab.Distribution">
           <DistributionCategoryWidget class="px-4 mb-4" />
           <AssetDetailSidebarDistribution
+            v-if="typeHasDistributions"
             :asset-id="assetId"
             :is-active="activeTab === AssetDetailTab.Distribution"
             :asset-type="assetType"
