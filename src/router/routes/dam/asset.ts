90d9c54834b83933f5d0f600c6b572c6a9b8434b
Stanislav Volar <stanislav.volar@gmail.com>
2023-02-06 16:34:21 +0100

    asset detail wip

--- src/router/routes/dam/asset.ts
+++ src/router/routes/dam/asset.ts
@@ -1,11 +1,23 @@
 import { ROUTE } from '@/router/routes'
 import { ACL } from '@/types/Permission'
 
+const PATH = '/asset'
+
 export const assetRoutes = [
   {
-    path: '/asset/list',
+    path: PATH + '/list',
     name: ROUTE.DAM.ASSET.LIST,
     component: () => import('@/views/dam/asset/list/AssetListView.vue'),
+    meta: {
+      requiresAuth: true,
+      requiredPermissions: [ACL.DAM_ASSET_VIEW],
+      layout: 'AppLayoutMain',
+    },
+  },
+  {
+    path: PATH + '/:id',
+    name: ROUTE.DAM.ASSET.DETAIL,
+    component: () => import('@/views/dam/asset/detail/AssetDetailView.vue'),
     meta: {
       requiresAuth: true,
       requiredPermissions: [ACL.DAM_ASSET_VIEW],
--- src/views/dam/asset/detail/composables/assetDetailActions.ts
+++ src/views/dam/asset/detail/composables/assetDetailActions.ts
@@ -1,10 +1,19 @@
 import { useAssetDetailStore } from '@/stores/dam/assetDetailStore'
 import { storeToRefs } from 'pinia'
-import { readonly } from 'vue'
+import { computed, readonly, ref } from 'vue'
+import { AssetType } from '@/model/dam/valueObject/AssetType'
+import { AssetStatus } from '@/model/dam/valueObject/AssetStatus'
+import { isImageFile } from '@/types/dam/File'
 
 export function useAssetDetailActions() {
+  const sidebar = ref(true)
+
   const assetDetailStore = useAssetDetailStore()
   const { asset, loader, metadataAreTouched } = storeToRefs(assetDetailStore)
+
+  const toggleSidebar = () => {
+    sidebar.value = !sidebar.value
+  }
 
   const metadataTouch = () => {
     metadataAreTouched.value = true
@@ -12,12 +21,76 @@ export function useAssetDetailActions() {
   const metadataUnTouch = () => {
     metadataAreTouched.value = false
   }
+
+  const assetType = computed(() => {
+    return asset.value?.attributes.assetType || AssetType.Default
+  })
+
+  const assetStatus = computed(() => {
+    if (!asset.value) return AssetStatus.Default
+    return asset.value.attributes.assetStatus
+  })
+
+  const isTypeImage = computed(() => {
+    return assetType.value === AssetType.Image
+  })
+  const isTypeVideo = computed(() => {
+    return assetType.value === AssetType.Video
+  })
+  const isTypeAudio = computed(() => {
+    return assetType.value === AssetType.Audio
+  })
+  const isTypeDocument = computed(() => {
+    return assetType.value === AssetType.Document
+  })
+
+  const assetMainFile = computed(() => {
+    return asset.value?.mainFile || undefined
+  })
+
+  const imageProperties = computed(() => {
+    if (asset.value?.mainFile && asset.value.mainFile.links && asset.value.mainFile.links.image_detail) {
+      return {
+        url: asset.value.mainFile.links.image_detail.url,
+        width: asset.value.mainFile.links.image_detail.width,
+        height: asset.value.mainFile.links.image_detail.height,
+        bgColor:
+          isImageFile(asset.value.mainFile) &&
+          asset.value.mainFile.imageAttributes &&
+          asset.value.mainFile.imageAttributes.mostDominantColor
+            ? asset.value.mainFile.imageAttributes.mostDominantColor
+            : '#ccc',
+      }
+    }
+    return {
+      url: '',
+      width: 356,
+      height: 200,
+      bgColor: '#ccc',
+    }
+  })
+
+  const toolbarTitle = computed(() => {
+    if (!asset.value) return ''
+    return asset.value.texts.displayTitle
+  })
+
   return {
+    sidebar,
+    toggleSidebar,
     asset,
     loader,
-
     metadataAreTouched: readonly(metadataAreTouched),
     metadataTouch,
     metadataUnTouch,
+    assetType,
+    assetStatus,
+    isTypeImage,
+    isTypeVideo,
+    isTypeAudio,
+    isTypeDocument,
+    imageProperties,
+    assetMainFile,
+    toolbarTitle,
   }
 }
