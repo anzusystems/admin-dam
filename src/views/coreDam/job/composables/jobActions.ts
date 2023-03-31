3a668dc1c44849f4fac92cb3e390ea597722dc0d
volar <stanislav.volar@petitpress.sk>
2023-03-31 17:23:17 +0200

    wip

--- src/views/coreDam/job/composables/jobActions.ts
+++ src/views/coreDam/job/composables/jobActions.ts
@@ -1,10 +1,10 @@
 import { ref } from 'vue'
-import type { Job } from '@/types/coreDam/Job'
 import { type FilterBag, type Pagination, useAlerts, useJobApi } from '@anzusystems/common-admin'
 import { damClient } from '@/services/api/clients/damClient'
 import { SYSTEM_CORE_DAM } from '@/model/systems'
 import { useJobOneStore } from '@/stores/coreDam/jobStore'
 import { storeToRefs } from 'pinia'
+import type { Job } from '@/types/coreDam/Job'
 
 const { showErrorsDefault } = useAlerts()
 
@@ -15,7 +15,7 @@ const detailLoading = ref(false)
 const { fetchJobList, fetchJob } = useJobApi<Job>(damClient, SYSTEM_CORE_DAM)
 
 export const useJobListActions = () => {
-  const listItems = ref<Job[]>([])
+  const listItems = ref<Array<Job>>([])
 
   const fetchList = async (pagination: Pagination, filterBag: FilterBag) => {
     listLoading.value = true
