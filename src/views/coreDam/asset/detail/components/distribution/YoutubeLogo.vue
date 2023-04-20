37992bc600d83ed0d934ff649677c492809da0a5
volar <stanislav.volar@petitpress.sk>
2023-04-20 16:15:28 +0200

    yt logo wip

---
+++ src/views/coreDam/asset/detail/components/distribution/YoutubeLogo.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { Theme, useTheme } from '@anzusystems/common-admin'
+import logoDark from '@/assets/yt_logo_rgb_dark.png'
+import logoLight from '@/assets/yt_logo_rgb_light.png'
+
+const { theme } = useTheme()
+</script>
+
+<template>
+  <div class="yt-logo">
+    <img
+      v-if="theme === Theme.Dark"
+      alt="YouTube"
+      :src="logoDark"
+    >
+    <img
+      v-else
+      alt="YouTube"
+      :src="logoLight"
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.yt-logo img {
+  height: 38px;
+  display: block;
+  width: auto;
+}
+</style>
