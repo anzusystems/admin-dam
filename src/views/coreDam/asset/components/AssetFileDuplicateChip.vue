e3af160a4349d3aeecd5efed73647351e3bb3ee1
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2023-06-02 15:47:39 +0200

    ref #72837 translations, Duplicate chip in slot, asset table slots chip, alt image view.

---
+++ src/views/coreDam/asset/components/AssetFileDuplicateChip.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+</script>
+
+<template>
+  <VChip
+    label
+    size="small"
+    color="warning"
+  >
+    {{ t('coreDam.asset.queueItem.duplicate') }}
+  </VChip>
+</template>
