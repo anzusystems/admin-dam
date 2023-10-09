34837f402b4f2e112d79ebc8f24d58822676b6e7
Stanislav Volar <stanislav.volar@gmail.com>
2023-10-09 09:52:09 +0200

    Hotfix/custom form element key rename (#143)
    * rename element key to property + make api safe to consume both variants
    
    * rename element key to property + make api safe to consume both variants
    
    ---------
    
    Co-authored-by: volar <stanislav.volar@petitpress.sk>

--- src/services/api/coreDam/assetCustomFormApi.ts
+++ src/services/api/coreDam/assetCustomFormApi.ts
@@ -3,7 +3,9 @@ import { apiFetchOne } from '@anzusystems/common-admin'
 import { damClient } from '@/services/api/clients/damClient'
 import { SYSTEM_CORE_DAM } from '@/model/systems'
 import type { AssetType } from '@/model/coreDam/valueObject/AssetType'
-import type { DamConfigAssetCustomFormElement } from '@/types/coreDam/DamConfigAssetCustomForm'
+import type {
+  DamConfigAssetCustomFormElementTemp
+} from '@/types/coreDam/DamConfigAssetCustomForm'
 import type { DistributionServiceName } from '@/types/coreDam/DamConfig'
 
 const END_POINT = '/adm/v1/asset-custom-form'
@@ -11,7 +13,8 @@ const ENTITY = 'assetCustomForm'
 
 // todo limit set to 100 for now, add load for pagination?
 export const fetchAssetCustomFormElements = (extSystem: IntegerId, assetType: AssetType) =>
-  apiFetchOne<{ data: DamConfigAssetCustomFormElement[] }>(
+  // apiFetchOne<{ data: DamConfigAssetCustomFormElement[] }>(
+  apiFetchOne<{ data: DamConfigAssetCustomFormElementTemp[] }>(
     damClient,
     END_POINT + '/ext-system/:extSystem/type/:assetType/element?order[position]=asc&limit=100',
     { extSystem, assetType },
@@ -21,7 +24,8 @@ export const fetchAssetCustomFormElements = (extSystem: IntegerId, assetType: As
 
 // todo limit set to 100 for now, add load for pagination?
 export const fetchDistributionCustomFormElements = (distributionService: DistributionServiceName) =>
-  apiFetchOne<{ data: DamConfigAssetCustomFormElement[] }>(
+  // apiFetchOne<{ data: DamConfigAssetCustomFormElement[] }>(
+  apiFetchOne<{ data: DamConfigAssetCustomFormElementTemp[] }>(
     damClient,
     END_POINT + '/distribution-service/:distributionService/element?order[position]=asc&limit=100',
     { distributionService },
