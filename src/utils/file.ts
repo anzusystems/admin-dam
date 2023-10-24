162286a96edf057c2c0e143692dad6b1bd35adad
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2023-10-24 13:23:28 +0200

    feature #74657 fixes prettyBps

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,13 +1,14 @@
 import dayjs from 'dayjs'
 
 export const prettyBps = (bytes: number, decimals = 2): string => {
+  const sizes = ['bps', 'kbps', 'Mbps', 'Gbps']
+
   if (bytes === 0) {
-    return '0 bps'
+    return '0 ' + sizes[0]
   }
 
   const k = 1024
   const dm = decimals < 0 ? 0 : decimals
-  const sizes = ['kbps', 'Mbps', 'Gbps']
   const i = Math.floor(Math.log(bytes) / Math.log(k))
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
