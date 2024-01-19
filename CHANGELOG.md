f30af7b5fa5b937eff6e4eb9df6ae8137d4fed6b
TomasHermanek <tomhermanek@gmail.com>
2024-01-19 10:28:55 +0100

    `MakeImagePublic` action now does not need any payload (#171)
    * `MakeImagePublic` action now does not need any payload
    
    * clickable assetId chip in `VideoShowEpisodeDetail`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [1.3.1](https://github.com/anzusystems/admin-dam/compare/1.2.0...1.3.0) (2024-01-19)
+
+### Features
+* `MakeImagePublic` action now does not need any payload
+* clickable assetId chip in `VideoShowEpisodeDetail` 
+
 ## [1.3.0](https://github.com/anzusystems/admin-dam/compare/1.2.0...1.3.0) (2024-01-10)
 
 ### Features
--- src/views/coreDam/videoShowEpisode/components/VideoShowEpisodeDetail.vue
+++ src/views/coreDam/videoShowEpisode/components/VideoShowEpisodeDetail.vue
@@ -1,16 +1,23 @@
 <script lang="ts" setup>
-import { ACopyText, ARow, AUserAndTimeTrackingFields } from '@anzusystems/common-admin'
+import { ACopyText, ARow, AUserAndTimeTrackingFields, COMMON_CONFIG } from '@anzusystems/common-admin'
 import { useI18n } from 'vue-i18n'
 import { storeToRefs } from 'pinia'
 import { useVideoShowEpisodeOneStore } from '@/stores/coreDam/videoShowEpisodeStore'
+import { ROUTE } from '@/router/routes'
+import { useRouter } from 'vue-router'
 
 const { videoShowEpisode } = storeToRefs(useVideoShowEpisodeOneStore())
 
 const { t } = useI18n()
+const router = useRouter()
 
 // const imageSrc = computed(() => {
 //   return videoShowEpisode.value.links ? videoShowEpisode.value.links.image_list.url : undefined
 // })
+
+const onAssetChipClick = () => {
+  router.push({ name: ROUTE.DAM.ASSET.DETAIL, params: { id: videoShowEpisode.value.asset } })
+}
 </script>
 
 <template>
@@ -33,7 +40,14 @@ const { t } = useI18n()
       <!--          <AssetImage :src="imageSrc" use-component />-->
       <!--        </ARow>-->
       <ARow :title="t('coreDam.videoShowEpisode.model.asset')">
-        {{ videoShowEpisode.asset }}
+        <VChip
+          size="small"
+          :append-icon="COMMON_CONFIG.CHIP.ICON.LINK"
+          label
+          @click.stop="onAssetChipClick"
+        >
+          {{ videoShowEpisode.asset }}
+        </VChip>
       </ARow>
       <AUserAndTimeTrackingFields :data="videoShowEpisode" />
     </VCol>
