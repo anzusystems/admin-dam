29eb74eed941137294d70b1870c2dae76acad794
volar <stanislav.volar@petitpress.sk>
2026-02-09 11:07:48 +0100

    fix title

--- src/views/coreDam/distributionCategory/composables/distributionCategoryValidation.ts
+++ src/views/coreDam/distributionCategory/composables/distributionCategoryValidation.ts
@@ -9,6 +9,7 @@ const { required, minLength } = useValidate()
 export function useDistributionCategoryValidation(distributionCategory: Ref<DistributionCategory>) {
   const rules = computed(() => ({
     distributionCategory: {
+      type: {},
       name: {
         required,
         minLength: minLength(2),
